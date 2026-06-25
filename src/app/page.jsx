3933104080db9b7f10fe8b2915ec00c7073be427
Taha11 <alphazack37@gmail.com>
2026-06-25 16:24:23 +0500

    Error

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,10 @@
 // app/page.jsx
-import Link from 'next/link';  // ← Add this import
+import Link from 'next/link';  
 import Navbar from "./components/navbar";
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       
     </>
   );
