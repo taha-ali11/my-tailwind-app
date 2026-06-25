8c421f19746d752a5e2bb6e8862ebfa6a6a8f022
Taha11 <alphazack37@gmail.com>
2026-06-25 21:13:46 +0500

    TextTyping Animation build-up

--- src/app/Sections/Hero.jsx
+++ src/app/Sections/Hero.jsx
@@ -1,5 +1,5 @@
 'use client';
-
+import TextType from '@/app/components/ui/Texttype';
 export default function Hero() {
     return (
         <>
@@ -19,16 +19,33 @@ export default function Hero() {
                     <h1 className="text-blue-950 text-7xl font-serif font-bold leading-tight">
                         DevX
                     </h1>
-                    <p className="text-blue-950 text-2xl font-serif mt-4">
-                        Software Development Company
-                    </p>
+                    <div className="text-blue-950 text-2xl font-serif mt-4">
+                        <TextType 
+    text={[
+        "Software Development Company",
+        "Building Digital Solutions", 
+        "Innovating the Future"
+    ]}
+    typingSpeed={50}
+    pauseDuration={2000}
+    deletingSpeed={30}
+    showCursor={true}
+    cursorCharacter="|"
+    className="text-blue-950 text-2xl font-serif"
+    cursorClassName="text-blue-950 text-2xl font-serif"
+    loop={true}
+    initialDelay={500}
+    variableSpeed={{ min: 40, max: 80 }}
+  cursorBlinkDuration={0.5} 
+              />
+                    </div>
                 </div>
 
                 {/* Image Area */}
                 <div className="absolute bottom-50 right-16">
                     <div className=" h-64 w-[500px] drop-shadow-2xl">
                         <div className="w-full h-full flex items-center justify-center text-blue-950 font-serif text-xl">
-                            <img src="/Assets/FL.png" alt="" />
+                            <img src="/Assets/FL.png" alt="IMG" />
                         </div>
                     </div>
                 </div>
